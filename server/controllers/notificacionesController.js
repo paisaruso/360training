6c281afb662614e36c584488c57009aedc3ef9f3
unknown <ricardo.osorioc@udea.edu.co>
2025-02-07 14:54:01 -0500

    Correccion notificaciones

--- server/controllers/notificacionesController.js
+++ server/controllers/notificacionesController.js
@@ -83,13 +83,13 @@ const markAllAsRead = async (req, res) => {
 };
 
 const getUnreadNotificationsCount = async (req, res) => {
-    const { id_deportista } = req.params;
+    const { id_usuario } = req.params;
     try {
         const result = await pool.query(
             `SELECT COUNT(*) AS unread_count
              FROM notificaciones
-             WHERE id_deportista = $1 AND leido = false`,
-            [id_deportista]
+             WHERE id_usuario = $1 AND leido = false`,
+            [id_usuario]
         );
         res.status(200).json(result.rows[0]);
     } catch (error) {
